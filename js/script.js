02389355dc388ae23c9db2b6055634cda88bbc2d
Kelvin Lau <kclau95@gmail.com>
2016-05-17 20:33:56 +1200

    stop doing things i don't need to do

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(evt){
   var menuItem = $("ul.menu > li");
   menuItem.height(menuItem.width());
+
+  var quickbutton = $("#quickbutton");
+  quickbutton.width(quickbutton.height());
 });
+
+function switchArrow() {
+  var quickicon = $("#quickicon");
+  if(quickicon.hasClass("fa-arrow-up")){
+    quickicon.removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  } else if(quickicon.hasClass("fa-arrow-down")) {
+    quickicon.removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  }
+}
