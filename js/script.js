f62bd9d448c38c173633a3af4c6031b8db46b8f0
Kelvin Lau <kclau95@gmail.com>
2016-05-22 02:02:36 +1200

    modal and js

--- js/script.js
+++ js/script.js
@@ -7,33 +7,41 @@ document.addEventListener("DOMContentLoaded", function(evt){
 
 
   $("#oven-toggle").click(function(){
-    var btn = $(this);
-
-    // btn.attr("data-target", "#oven-" + btn.text().toLowerCase() + "-tab");
-    btn.text(btn.text() === "OFF" ? "ON" : "OFF");
-    btn.toggleClass("btn-danger btn-success");
-
-    var isDisabled = $("#oven-fields").prop("disabled");
-
-    if (isDisabled) {
-      $("#oven-fields").prop("disabled", false);
-    } else {
-      $("#oven-fields").prop("disabled", true);
-    }
-
-    // $("#oven-off-tab, #oven-on-tab").toggleClass("in active");
-
-    // if (btn.text() === "OFF"){
-    //   btn.text("ON");
-    //   btn.attr("target", "#oven-off-tab");
-    // } else {
-    //   btn.text("OFF");
-    //   btn.attr("target", "#oven-on-tab");
-    // }
-
+    toggleOven();
+    $(this).toggleClass("btn-danger btn-success");
   });
 
+  $('#ovenModal').on('hide.bs.modal', function (e) {
+    console.log("hidden");
+    // do something...
+  });
 });
+
+function toggleOven() {
+  var btn = $("#oven-toggle");
+
+  // btn.attr("data-target", "#oven-" + btn.text().toLowerCase() + "-tab");
+  btn.text(btn.text() === "OFF" ? "ON" : "OFF");
+
+  var isDisabled = $("#oven-fields").prop("disabled");
+
+  if (isDisabled) {
+    $("#oven-fields").prop("disabled", false);
+  } else {
+    $("#oven-fields").prop("disabled", true);
+  }
+
+  // $("#oven-off-tab, #oven-on-tab").toggleClass("in active");
+
+  // if (btn.text() === "OFF"){
+  //   btn.text("ON");
+  //   btn.attr("target", "#oven-off-tab");
+  // } else {
+  //   btn.text("OFF");
+  //   btn.attr("target", "#oven-on-tab");
+  // }
+
+}
 
 function switchArrow() {
   var quickicon = $("#quickicon");
