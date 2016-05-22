ae47f0a3d385ce1d66872bb86f3e9b79c1ecedc5
Kelvin Lau <kclau95@gmail.com>
2016-05-22 16:16:32 +1200

    a lot of shit

--- js/script.js
+++ js/script.js
@@ -11,11 +11,52 @@ document.addEventListener("DOMContentLoaded", function(evt){
     $(this).toggleClass("btn-danger btn-success");
   });
 
-  $('#ovenModal').on('hide.bs.modal', function (e) {
-    console.log("hidden");
-    // do something...
+  $('#ovenModal').on('hidden.bs.modal', function (e) {
+    if(!savedOven){
+      if(ovenStatus !== $("#oven-toggle").text()){
+        // loadSnapshot();
+        $("#oven-toggle").click();
+      }
+    }
   });
+
+  $("#ovenModal").on('show.bs.modal', function(e){
+    savedOven = false;
+    ovenStatus = $("#oven-toggle").text();
+    // ovenSnapshot();
+  });
+
+  $("#oven-form").submit(function(e){
+    $('#ovenModal').modal('hide');
+    savedOven = true;
+
+    if ($("#oven-toggle").text() === "ON" ) {
+      createOnAlert();
+
+      $("#oven-block").css("fill", "var(--light3)");
+      $("#oven-block").hover(function(){
+        $(this).css("fill", "var(--norm1)");
+      },function(){
+        $(this).css("fill", "var(--light3)");
+      });
+
+    } else {
+
+      $("#oven-block").css("fill", "white");
+      $("#oven-block").hover(function(){
+        $(this).css("fill", "var(--norm1)");
+      },function(){
+        $(this).css("fill", "white");
+      });
+
+    }
+
+    e.preventDefault();
+  });
+
 });
+var savedOven;
+var ovenStatus;
 
 function toggleOven() {
   var btn = $("#oven-toggle");
@@ -24,7 +65,6 @@ function toggleOven() {
   btn.text(btn.text() === "OFF" ? "ON" : "OFF");
 
   var isDisabled = $("#oven-fields").prop("disabled");
-
   if (isDisabled) {
     $("#oven-fields").prop("disabled", false);
   } else {
@@ -32,22 +72,44 @@ function toggleOven() {
   }
 
   // $("#oven-off-tab, #oven-on-tab").toggleClass("in active");
-
-  // if (btn.text() === "OFF"){
-  //   btn.text("ON");
-  //   btn.attr("target", "#oven-off-tab");
-  // } else {
-  //   btn.text("OFF");
-  //   btn.attr("target", "#oven-on-tab");
-  // }
+  /*if (btn.text() === "OFF"){
+    btn.text("ON");
+    btn.attr("target", "#oven-off-tab");
+  } else {
+    btn.text("OFF");
+    btn.attr("target", "#oven-on-tab");
+  }*/
 
 }
 
+/*var ovenTemp;
+var tempUnit;
+var ovenTime;
+var ovenMode;
+
+function ovenSnapshot(){
+  ovenTemp = $("#oven-temp").val();
+  tempUnit = $("input:radio[name=unit]:checked").val();
+  ovenTime = $("#oven-time").val();
+  ovenMode = $("#oven-mode").val();
+}
+
+function loadSnapshot(){
+  $("#oven-temp").val(ovenTemp);
+  $("input:radio[name=unit][value=" + tempUnit + "]").prop("checked", true);
+  $("#oven-time").val(ovenTime);
+  $("#oven-mode").val(ovenMode);
+}*/
+
+function createOnAlert() {
+  $("#internal-pane").after(
+    '<div id="oven-alert" class="alert alert-success alert-dismissible overlay" role="alert" >' +
+      '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span></button>' +
+      'The oven has been turned on at '+ $("#oven-temp").val() + ' &deg;' + $("input:radio[name=unit]:checked").val() + ' for ' + $("#oven-time").val() + ' minutes on ' + $("#oven-mode").val() + ' mode.' +
+    '</div>'
+  );
+}
+
 function switchArrow() {
-  var quickicon = $("#quickicon");
-  if(quickicon.hasClass("fa-arrow-up")){
-    quickicon.removeClass("fa-arrow-up").addClass("fa-arrow-down");
-  } else if(quickicon.hasClass("fa-arrow-down")) {
-    quickicon.removeClass("fa-arrow-down").addClass("fa-arrow-up");
-  }
+  $("#quickicon").toggleClass("fa-arrow-up fa-arrow-down");
 }
